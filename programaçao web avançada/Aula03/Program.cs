43ca67cbca68271f229998d499bb963d9d6af186
Laura Nagen <137368966+nananagen@users.noreply.github.com>
2024-02-23 21:47:29 -0300

    Add files via upload

---
+++ programaçao web avançada/Aula03/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.Sqlite;
+
+using(var conexao = new SqliteConnection("db/dados.db"))
+{
+    conexao.Open();
+
+    var cmd = conexao.CreateCommand();
+    cmd.CommandText =   "SELECT * FROM contato" ;
+    var dr = cmd.ExecuteReader();
+
+    while(dr.Read()){
+        Console.WriteLine($"{dr["nome"]}: {dr["email"]}");
+    }
+    dr.Close();
+}
+/*
+using(var conexao = new SqliteConnection("db/dados.db"))
+{
+    conexao.Open();
+
+    var cmd = conexao.CreateCommand();
+    /*
+    cmd.CommandText =   "INSERT INTO contato"  +
+                        "(id, nome, email)" +
+                        "VALUES (@id, @nome, @email)";
+    
+    cmd.CommandText = "DELETE FROM contato WHERE id=@id";
+    cmd.Parameters.AddWithValue("@id", 4);
+    
+    cmd.Parameters.AddWithValue("@nome", "Daniel da Silva");
+    cmd.Parameters.AddWithValue("@email",
+                                "daniel@email.com");
+    
+    cmd.ExecuteNonQuery();
+} */
